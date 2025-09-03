ef8068ceb8b7a32922c086114fd5336c61f132a0
Richard <proxyserver7798@gmail.com>
2025-09-03 11:03:04 +0200

    Day start commit

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
       <MainNavigation />
       <Banner />
       <About />
-      {/*<WhyChooseUs />
+      <WhyChooseUs />
       <Services />
-      <Testimonial />
+      {/*<Testimonial />
       <ContactUs />
       <Newsletter />
       <Footer /> */}
--- components/main-navigation.jsx
+++ components/main-navigation.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import styles from "./comp-styles/main-navigation.module.css";
 import Link from "next/link";
@@ -8,7 +8,6 @@ import { RiMenu3Fill } from "react-icons/ri";
 import { MdClose } from "react-icons/md";
 import { useState } from "react";
 
-
 export default function MainNavigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   function toggleMenu() {
@@ -16,18 +15,32 @@ export default function MainNavigation() {
   }
   return (
     <nav className={styles.main_nav_container}>
-      <div className={styles.mobile_nav_overlay} style={{ display: isMenuOpen ? 'block' : 'none' }}>
-        <button className={styles.close_menu_button} onClick={() => toggleMenu()}>
+      <div
+        className={styles.mobile_nav_overlay}
+        style={{ display: isMenuOpen ? "block" : "none" }}
+      >
+        <button
+          className={styles.close_menu_button}
+          onClick={() => toggleMenu()}
+        >
           <MdClose size={25} className={styles.close_menu_icon} />
         </button>
         <div className={styles.mobile_nav_prop}>
           <p>Shadow Watch</p>
         </div>
         <ul className={styles.mobile_nav_list}>
-          <li onClick={() => toggleMenu()}><Link href="#home">Home</Link></li>
-          <li onClick={() => toggleMenu()}><Link href="#about">About</Link></li>
-          <li onClick={() => toggleMenu()}><Link href="#services">Services</Link></li>
-          <li onClick={() => toggleMenu()}><Link href="#contact">Contact</Link></li>
+          <li onClick={() => toggleMenu()}>
+            <Link href="#home">Home</Link>
+          </li>
+          <li onClick={() => toggleMenu()}>
+            <Link href="#about">About</Link>
+          </li>
+          <li onClick={() => toggleMenu()}>
+            <Link href="#services">Services</Link>
+          </li>
+          <li onClick={() => toggleMenu()}>
+            <Link href="#contact">Contact</Link>
+          </li>
         </ul>
       </div>
       <ul className={styles.nav_list}>
@@ -46,10 +59,15 @@ export default function MainNavigation() {
             Services
           </Link>
         </li>
-        <button className={`${styles.nav_cta_appointment} ${styles.nav_cta} ${styles.hidden_on_desktop}`}>
+        <button
+          className={`${styles.nav_cta_appointment} ${styles.nav_cta} ${styles.hidden_on_desktop}`}
+        >
           Book Appointment
         </button>
-        <button className={`${styles.hidden_on_desktop}`} onClick={() => toggleMenu()}>
+        <button
+          className={`${styles.hidden_on_desktop}`}
+          onClick={() => toggleMenu()}
+        >
           <RiMenu3Fill size={30} className={styles.hamburger_menu_icon} />
         </button>
       </ul>
@@ -62,11 +80,15 @@ export default function MainNavigation() {
         />
       </div>
       <div className={styles.nav_cta_container}>
-        <button className={`${styles.nav_cta_contact} ${styles.nav_cta} ${styles.hide_at_mobile}`}>
+        <button
+          className={`${styles.nav_cta_contact} ${styles.nav_cta} ${styles.hide_at_mobile}`}
+        >
           <p>Contact Us</p>
           <IoMdPlayCircle size={20} />
         </button>
-        <button className={`${styles.nav_cta_appointment} ${styles.nav_cta} ${styles.hide_at_mobile}`}>
+        <button
+          className={`${styles.nav_cta_appointment} ${styles.nav_cta} ${styles.hide_at_mobile}`}
+        >
           Book Appointment
         </button>
       </div>
